1e248c66a9e19f8552587d121e75bd201ae5ef33
techkilla2016 <66670136+techkilla2016@users.noreply.github.com>
2024-02-11 20:12:06 +0530

    Update page.js

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -61,7 +61,7 @@ const Home = () => {
         try {
             // console.log(imgFile)
             // axios.post('https://1a53-103-17-110-126.ngrok-free.app/rec', {
-            axios.post('https://911c-103-17-110-127.ngrok-free.app/rec', {
+            axios.post('https://76ec-103-17-110-127.ngrok-free.app/img2img', {
                 image: imgFile.split(',')[1],
                 choice: select
             }).then(res => {
